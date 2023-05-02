94fc1b32705d40907c1ae2255caf7820a5387473
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-05-02 18:59:02 -0300

    models.py

--- confApp/models.py
+++ confApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.TextField(),
+    description = models.TextField()
+
+
+class Author(models.Model):
+    first_name = models.TextField(),
+    last_name = models.TextField()
+
+
+class New(models.Model):
+    title = models.TextField(),
+    body = models.TextField(),
+    publish_date = models.DateField(),
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
