5022ab4e487ea4793842c0801686fff445679123
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-05-27 20:59:39 -0300

    load images in all screen views

--- conf/settings.py
+++ conf/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-MEDIA_ROOT = ''
-MEDIA_URL = ''
+MEDIA_URL = '/confApp/news_images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'confApp/news_images')
 
 
 # Quick-start development settings - unsuitable for production
